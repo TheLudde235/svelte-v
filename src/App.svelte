f5337b8eb3dfc9183bda1d6ea103a79e034d5ce9
TheLudde235 <ludde.lagerstrom@outlook.com>
2023-04-04 18:55:54 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+    const modules = import.meta.glob('./funnies/*.wav');
+    const funnies = [];
+    for (const path in modules) {
+        funnies.push(path)
+    }
+</script>
+
+<main>
+    <input type="text" placeholder="pop-up content" id="popup-input">
+    <a href="#"><h1>Copy pop-up (run)</h1></a>
+    {#each funnies as funny}
+    <div class="funny">
+        <p>{funny.split('/')[2]}</p>
+        <audio src="/src/{funny}" controls></audio> <br>
+        <a href="#">copy open-program script (ps1)</a>
+        <hr>
+    </div>
+    {/each}
+
+    <a href="#">Clear</a>
+</main>
+
+<style>
+    a {
+        color: blue;
+    }
+
+    input#popup-input {
+        display: flex;
+        width: 98%;
+        margin: 10px auto;
+    }
+    
+    .funny {
+        margin: 10px;
+    }
+    
+    h1, a {
+        margin: 10px;
+    } 
+</style>
