94589de73102c64589f524055e85b099385b0daf
TheLudde235 <ludde.lagerstrom@outlook.com>
2023-04-04 20:21:34 +0200

    feat: pop-up run script

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,48 @@
 <script>
-    const modules = import.meta.glob('./funnies/*.wav');
+// @ts-nocheck
+
+    const modules = import.meta.glob("./funnies/*.wav");
     let funnies = [];
-    
+
+    let popupContent = "";
+
     for (const path in modules) {
-        modules[path]().then(m => {
-            console.log(m.default)
+        modules[path]().then((m) => {
+            console.log(m.default);
             funnies = [...funnies, m.default];
         });
     }
 
     function copyScript(file) {
-        const name = file.split('/')[2];
+        const name = file.split("/")[2];
         const text = `if(-not (Test-Path -Path  \"$env:USERPROFILE\\${name}\")){Invoke-WebRequest -Uri \"https://v.theludde235.se/assets/${name}\" -OutFile \"$env:USERPROFILE\\${name}\";}Set-ItemProperty -Path \"HKCU:\\AppEvents\\Schemes\\Apps\\.Default\\Open\\.Current\" -Name \"(Default)\" -Type \"String\" -Value \"$env:USERPROFILE\\${name}\";`;
         navigator.clipboard.writeText(text);
     }
-  
-//   document.querySelector('a:not(.sound)').addEventListener('click', () => {
-//     const text = document.querySelector('input').value;
-//     navigator.clipboard.writeText(
-//       `C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -noexit -ExecutionPolicy Bypass "Add-Type -AssemblyName System.Windows.Forms;[System.Windows.Forms.MessageBox]::Show('${text}');"`
-//     );
-//   });
+
+    function copyPopup(text) {
+        navigator.clipboard.writeText(
+            `C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -noexit -ExecutionPolicy Bypass "Add-Type -AssemblyName System.Windows.Forms;[System.Windows.Forms.MessageBox]::Show('${text}');"`
+        );
+    }
 </script>
 
 <main>
-    <input type="text" placeholder="pop-up content" id="popup-input">
-    <a href="#"><h1>Copy pop-up (run)</h1></a>
+    <input
+        type="text"
+        placeholder="pop-up content"
+        id="popup-input"
+        bind:value={popupContent}
+    />
+    <a href="#" on:click={copyPopup(popupContent)}><h1>Copy pop-up (run)</h1></a>
     {#each funnies as funny}
-    <div class="funny">
-        <p>{funny.split('/')[2]}</p>
-        <audio src="{funny}" controls></audio> <br>
-        <a href="#" on:click={copyScript(funny)}>copy open-program script (ps1)</a>
-        <hr>
-    </div>
+        <div class="funny">
+            <p>{funny.split("/")[2]}</p>
+            <audio src={funny} controls /> <br />
+            <a href="#" on:click={copyScript(funny)}
+                >copy open-program script (ps1)</a
+            >
+            <hr />
+        </div>
     {/each}
 
     <a href="#">Clear</a>
@@ -48,12 +58,13 @@
         width: 98%;
         margin: 10px auto;
     }
-    
+
     .funny {
         margin: 10px;
     }
-    
-    h1, a {
+
+    h1,
+    a {
         margin: 10px;
-    } 
+    }
 </style>
